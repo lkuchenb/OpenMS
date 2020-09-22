bd342bf7e5983ca30e5f2ae9be2cd15796b8d104
Leon Kuchenbecker <kuchenb@informatik.uni-tuebingen.de>
2020-09-22 22:46:20 +0200

    [FIX] Resolve compatibility issues between IDRipper and IDMerger

--- src/openms/include/OpenMS/ANALYSIS/ID/IDRipper.h
+++ src/openms/include/OpenMS/ANALYSIS/ID/IDRipper.h
@@ -28,8 +28,8 @@
 // ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
 // --------------------------------------------------------------------------
-// $Maintainer: Timo Sachsenberg $
-// $Authors: Immanuel Luhn $
+// $Maintainer: Timo Sachsenberg$
+// $Authors: Immanuel Luhn, Leon Kuchenbecker$
 // --------------------------------------------------------------------------
 #pragma once
 
@@ -53,6 +53,60 @@ namespace OpenMS
     public DefaultParamHandler
   {
 public:
+    /// Possible input file encodings for the origin as used by different versions of IDMerger
+    enum OriginAnnotationFormat { FILE_ORIGIN = 0, MAP_INDEX = 1, ID_MERGE_INDEX = 2, UNKNOWN = 3, SIZE_OF_ORIGIN_ANNOTATION_FORMAT = 4 };
+
+    /// String representations for the OriginAnnotationFormat enum
+    static const std::array<std::string, SIZE_OF_ORIGIN_ANNOTATION_FORMAT> names_of_OriginAnnotationFormat;
+
+    /// Represents a set of IdentificationRuns
+    struct IdentificationRuns
+    {
+        /// Maps a unique index to every IdentificationRun string representation (getIdentifier()).
+        std::map<String, UInt> index_map;
+        /// Maps the list of spectra data elements to every IdentificationRun index.
+        std::vector<StringList> spectra_data;
+
+        /// Generates a new IdentificationRuns object from a vector of ProteinIdentification objects.
+        IdentificationRuns(const std::vector<ProteinIdentification>& prot_ids);
+    };
+
+    /// Identifies an IDRipper output file
+    struct RipFileIdentifier
+    {
+        /// The numerical index of the source IdentificationRun
+        UInt ident_run_idx;
+        /// The numerical index of the source file_origin / spectra_data element
+        UInt file_origin_idx;
+        /// The output basename derived from the file_origin / spectra_data element
+        String out_basename;
+        /// The full length origin read from the file_origin / spectra_data element
+        String origin_fullname;
+
+        /// Constructs a new RipFileIdentifier object
+        RipFileIdentifier(const IDRipper::IdentificationRuns& id_runs, const PeptideIdentification& pep_id, const std::map<String, UInt>& file_origin_map, const IDRipper::OriginAnnotationFormat origin_annotation_fmt, bool split_ident_runs);
+    };
+
+    /// Provides a 'less' operation for RipFileIdentifiers that ignores the out_basename and origin_fullname members
+    struct RipFileIdentifierIdxComparator
+    {
+        bool operator()(const RipFileIdentifier& left, const RipFileIdentifier& right) const;
+    };
+
+    /// Represents the content of an IDRipper output file
+    struct RipFileContent
+    {
+        /// Protein identifications
+        std::vector<ProteinIdentification> prot_idents;
+        /// Peptide identifications
+        std::vector<PeptideIdentification> pep_idents;
+        /// Constructs a new RipFileContent object
+        RipFileContent(const std::vector<ProteinIdentification>& prot_idents, const std::vector<PeptideIdentification>& pep_idents)
+            : prot_idents(prot_idents), pep_idents(pep_idents) {}
+    };
+
+    /// Represents the result of an IDRipper process, a map assigning file content to output file identifiers
+    typedef std::map<RipFileIdentifier, RipFileContent, RipFileIdentifierIdxComparator> RipFileMap;
 
     /// Default constructor
     IDRipper();
@@ -70,11 +124,10 @@ public:
       @param proteins Peptide identification annotated with file origin
       @param peptides Protein identification
     */
-    void rip(std::map<String, std::pair<std::vector<ProteinIdentification>, std::vector<PeptideIdentification> > > & ripped, std::vector<ProteinIdentification> & proteins, std::vector<PeptideIdentification> & peptides);
+    void rip(RipFileMap& ripped, std::vector<ProteinIdentification>& proteins, std::vector<PeptideIdentification>& peptides, bool full_split, bool split_ident_runs);
 
 private:
-
-    //Not implemented
+    // Not implemented
     /// Copy constructor
     IDRipper(const IDRipper & rhs);
 
@@ -82,12 +135,18 @@ private:
     /// Assignment
     IDRipper & operator=(const IDRipper & rhs);
 
+    /// helper function, detects file origin annotation standard from collections of protein and peptide hits
+    OriginAnnotationFormat detectOriginAnnotationFormat_(std::map<String, UInt> & file_origin_map, const std::vector<PeptideIdentification> & peptide_idents);
     /// helper function, extracts all protein hits that match the protein accession
     void getProteinHits_(std::vector<ProteinHit> & result, const std::vector<ProteinHit> & protein_hits, const std::vector<String> & protein_accessions);
     /// helper function, returns the string representation of the peptide hit accession
     void getProteinAccessions_(std::vector<String> & result, const std::vector<PeptideHit> & peptide_hits);
     /// helper function, returns the protein identification for the given peptide identification based on the same identifier
     void getProteinIdentification_(ProteinIdentification & result, PeptideIdentification pep_ident, std::vector<ProteinIdentification> & prot_idents);
+    /// helper function, register a potential output file basename to detect duplicate output basenames
+    bool registerBasename_(std::map<String, std::pair<UInt, UInt> >& basename_to_numeric, const IDRipper::RipFileIdentifier& rfi);
+    /// helper function, sets the value of mode to new_value and returns true if the old value was identical or unset (-1)
+    bool setOriginAnnotationMode_(short& mode, short const new_value);
   };
 
 } // namespace OpenMS
